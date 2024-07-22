6aec24bf5aff5bb934a730a6d65542e3775c758c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-22 21:52:47 +0000

    test

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0-simple_helper_function.py"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     Returns a tuple of size two containing a start index and an end index
