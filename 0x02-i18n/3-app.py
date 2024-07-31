38d447e3ac8ab50dc70f96896a1112b72226c6bc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-31 00:09:19 +0000

    test

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,12 +2,13 @@
 """
 Initialization of a Flask application with Babel for text localization.
 """
-
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 from typing import Optional
 
+
 app = Flask(__name__)
+
 
 class Config:
     """
@@ -17,8 +18,10 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale() -> Optional[str]:
@@ -27,12 +30,14 @@ def get_locale() -> Optional[str]:
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index() -> str:
     """
     Displays the home page with localization.
     """
     return render_template('3-index.html')
+
 
 if __name__ == "__main__":
     app.run()
