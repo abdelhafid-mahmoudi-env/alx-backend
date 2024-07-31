76808c59bdd23aa3bc0f5023c0e3b061f4c96fb0
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-31 00:14:15 +0000

    test

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,12 +2,13 @@
 """
 Initialization of a Flask application with Babel for text localization.
 """
-
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -15,6 +16,7 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 def get_user():
     """
@@ -25,12 +27,14 @@ def get_user():
         return users.get(int(user_id))
     return None
 
+
 @app.before_request
 def before_request():
     """
     Executes before each request to set the global user.
     """
     g.user = get_user()
+
 
 @app.route('/')
 def index():
@@ -39,12 +43,14 @@ def index():
     """
     return render_template('5-index.html')
 
+
 @babel.localeselector
 def get_locale():
     """
     Selects the appropriate locale based on the HTTP request.
     """
     return request.accept_languages.best_match(['en', 'fr'])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
