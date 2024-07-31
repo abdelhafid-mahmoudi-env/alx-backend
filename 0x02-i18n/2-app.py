3402f26ceded44ddee5efb245877539a8d8b4fa3
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-31 00:02:02 +0000

    test

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,12 +3,13 @@
 Initialization of a Flask application with Babel for
 language selection based on the user's request.
 """
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 from typing import Optional
 
+
 app = Flask(__name__)
+
 
 class Config:
     """
@@ -18,8 +19,10 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale() -> Optional[str]:
@@ -28,12 +31,14 @@ def get_locale() -> Optional[str]:
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index() -> str:
     """
     Displays the home page.
     """
     return render_template('2-index.html')
+
 
 if __name__ == "__main__":
     app.run()
